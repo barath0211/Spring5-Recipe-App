21fd6448296d4205eeb8868e66846fa31c9bedd2
barath0211 <barathkumaarabirami@gmail.com>
2022-10-31 14:10:13 +0530

    Spring Data JPA Query Methods

--- src/main/java/barath/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/barath/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,12 +1,32 @@
 package barath.springframework.spring5recipeapp.controllers;
 
+import barath.springframework.spring5recipeapp.domain.Category;
+import barath.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import barath.springframework.spring5recipeapp.repositories.CategoryRepository;
+import barath.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/","/index"})
     public String getIndexPage(){
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is" + categoryOptional.get().getId());
+        System.out.println("UOM Id is " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
