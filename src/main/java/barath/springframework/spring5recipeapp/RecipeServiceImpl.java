a922faee3498cb43365b5e7f6b9142445fdb5a67
barath0211 <barathkumaarabirami@gmail.com>
2022-11-05 12:40:37 +0530

    Adding BootStrap CSS

--- src/main/java/barath/springframework/spring5recipeapp/RecipeServiceImpl.java
+++ src/main/java/barath/springframework/spring5recipeapp/RecipeServiceImpl.java
@@ -3,11 +3,13 @@ package barath.springframework.spring5recipeapp;
 import barath.springframework.spring5recipeapp.domain.Recipe;
 import barath.springframework.spring5recipeapp.repositories.RecipeRepository;
 import barath.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("i'm in the Service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
--- src/main/java/barath/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/barath/springframework/spring5recipeapp/controllers/IndexController.java
@@ -5,12 +5,12 @@ import barath.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import barath.springframework.spring5recipeapp.repositories.CategoryRepository;
 import barath.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import barath.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -21,6 +21,7 @@ public class IndexController {
 
     @RequestMapping({"", "/","/index"})
     public String getIndexPage(Model model){
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
