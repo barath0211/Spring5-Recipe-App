54e2eb4017a06bfc73ebdaa95551c7ad2d87be7c
barath0211 <barathkumaarabirami@gmail.com>
2022-11-07 15:04:27 +0530

    Assignment Review

---
+++ src/test/java/barath/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package barath.springframework.spring5recipeapp.controllers;
+
+import barath.springframework.spring5recipeapp.services.RecipeService;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @Before("")
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
